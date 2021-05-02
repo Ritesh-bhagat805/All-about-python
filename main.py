04eb56bf07f82910f93c0c7bb121509ebe83a633
Ritesh Bhagat <83543821+Ritesh-bhagat805@users.noreply.github.com>
2021-05-02 09:24:11 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+print("ritesh bhagat")
+
+# int= number(1,2,3)
+# float= 12.0
+# string="ritesh"
+
+# print(10)
+# float(10.10)
+# string
+
+# int
+# print(10+10)
+# print(10-10)
+# print(10*10)
+# print(10/10)
+#
+# print(type(8))
+#
+# print(type("ritesh " + "bhagat"))
+#
+# a = 10.8
+# b = 20.8
+# print(a + b)
+
+# python is a interpreted language
+a = 'python'
+b = 'interpreted'
+
+print(f"{a} is a {b} language")
+
+r = 'python language'
+
+print(r[4])
+
+
+
+
+
